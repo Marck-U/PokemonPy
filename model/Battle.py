74a8b81f18c8f70a46dc55621e19eff101924c8b
Marco Azúa Ulloa <marcoazuabiobio@gmail.com>
2019-02-04 01:14:43 -0300

    Critico, debilidades
    Formular de critico y debilidades

--- model/Battle.py
+++ model/Battle.py
@@ -1,5 +1,5 @@
 from contants import*
-
+import random
 class Battle:
     def __init__(self, pokemon1, pokemon2):
         self.pokemon1 = pokemon1
@@ -11,6 +11,15 @@ class Battle:
         if finished:
             self.print_winner()
         return finished
+    
+    def print_winner(self):
+        if self.pokemon1.current_hp <= 0 < self.pokemon2.current_hp:
+            print(self.pokemon2.name + " Tú pokemon gano en " + str(self.actual_turn) + " turnos")
+
+        elif self.pokemon2.current_hp <= 0 < self.pokemon1.current_hp:
+            print(self.pokemon1.name + " Tú pokemon gano en " + str(self.actual_turn) + " turnos")
+        else:
+            print("Double KO!!!!")
 
     def execute_turn(self, turn):
         command1 = turn.command1
@@ -21,26 +30,47 @@ class Battle:
             attack1 = self.pokemon1.attacks[command1.action[DO_ATTACK]]
         if DO_ATTACK in command2.action.keys():
             attack2 = self.pokemon2.attacks[command2.action[DO_ATTACK]]
+    
         
         #Damage Formula
 
-        self.pokemon2.current_hp -= attack1.power
-        self.pokemon1.current_hp -= attack2.power
-
+        self.pokemon2.current_hp -= self.compute_damage(attack1, self.pokemon1, self.pokemon2)
+        self.pokemon1.current_hp -= self.compute_damage(attack1, self.pokemon2, self.pokemon1)
         self.actual_turn += 1
 
-    def print_winner(self):
-        if self.pokemon1.current_hp <= 0 < self.pokemon2.current_hp:
-            print(self.pokemon2.name + " Tú pokemon gano en " + str(self.actual_turn) + " turnos")
-
-        elif self.pokemon2.current_hp <= 0 < self.pokemon1.current_hp:
-            print(self.pokemon1.name + " Tú pokemon gano en " + str(self.actual_turn) + " turnos")
-        else:
-            print("Double KO!!!!")
-
+    #Vida
     def print_current_status(self):
         print(self.pokemon1.name + " tiene " + str(self.pokemon1.current_hp)+ " de vida!")
         print(self.pokemon2.name + " tiene " + str(self.pokemon2.current_hp)+ " de vida!")
+
+    def compute_damage(self, attack, pokemon1, pokemon2):
+        aux = ((2*pokemon1.level)/5) + 2
+        powerFactor = aux * attack.power
+        if attack.category == PHYSICAL:
+            print("Physical attack!")
+            powerFactor *= (pokemon1.stats[ATTACK]/pokemon2.stats[DEFENSE])
+        else:
+            powerFactor *= (pokemon1.stats[SPATTACK]/pokemon2.stats[SPDEFENSE])   
+        damage_without_modifier = powerFactor/50 + 2
+        finalDamage = damage_without_modifier * self.compute_damage_modifier(attack, self.pokemon1, self.pokemon2)
+        print(finalDamage)
+        return finalDamage
+    
+    def compute_damage_modifier(self, attack, pokemon1, pokemon2):
+        stab = 1
+        if attack.type == pokemon1.type or attack.type == pokemon1.type2:
+            print("HAS STAB")
+            stab = 1.5
+        effectiveness1 = TYPE_CHART[pokemon2.type1][attack.type]
+        effectiveness2 = TYPE_CHART[pokemon2.type2][attack.type]
+        effectiveness_final = effectiveness1 * effectiveness2
+
+        #computo del critico
+        critical = 1
+        if random.random() <= 0.1:
+            print(pokemon1.name, "did a critical attack!!")
+            critical = 1.5
+        return stab * effectiveness_final * critical
 
 class Turn:
     def __init__(self):
